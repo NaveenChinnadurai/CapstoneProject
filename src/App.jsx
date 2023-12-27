f063ca5e887ae354386e55013aa650427993d1b4
Naveen Chinnadurai <111662328+NaveenChinnadurai@users.noreply.github.com>
2023-12-27 23:51:03 +0530

    updated:home page,booking functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from './components/header'
 import BookingPage from './pages/bookingPage'
 import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/homepage'
-import PageUnderWork from './pages/PageUnderWork'
+import PageUnderWork from './pages/pageUnderWork'
 import ConfirmPage from './components/bookingPage/confirmPage'
 
 function App() {
